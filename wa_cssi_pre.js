173f8855674230157d1ff4fd6a7478589081dfd4
Dion D'aquino <daquinohd@users.noreply.github.com>
2015-12-08 18:39:25 +0000

    //tracker.nci.nih.gov/browse/OCEWAQ-569 - fixed unassigned wa_is_production_report_suite variable

--- wa_cssi_pre.js
+++ wa_cssi_pre.js
@@ -1,15 +1,17 @@
 var wa_production_report_suite = 'ncicssi-strategicscientificinitiatives';
 var wa_dev_report_suite = 'ncicssi-strategicscientificinitiativ-dev';
-var wa_channel = 'CSSI – Nano';
+var wa_channel = 'CSSI - Nano';
 var wa_search_function_name = 'CSSI - Nano - Search';
 var wa_production_url_match = 'nano.cancer.gov';
 var wa_dev_url_match = 'nano-dev.cancer.gov';
 var wa_production_linkInternalFilters = 'javascript:,nano.cancer.gov';
 var wa_dev_linkInternalFilters = 'javascript:,nano-dev.cancer.gov';
+var wa_is_production_report_suite = false;
 
 if (document.URL.indexOf(wa_production_url_match) != -1)
     // production 
     var s_account=wa_production_report_suite;
+	var wa_is_production_report_suite = true;
 else 
     // non-production
     var s_account=wa_dev_report_suite;
