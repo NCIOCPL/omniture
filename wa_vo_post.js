b25e033a6eaa4f64df8aa9810efe9ca526cca204
Dion D'aquino <daquinohd@users.noreply.github.com>
2016-04-20 13:09:02 +0000

    //tracker.nci.nih.gov/browse/OCEWAQ-645 - creating analytics Javascript for Visuals Online

---
+++ wa_vo_post.js
@@ -0,0 +1,18 @@
+﻿s.linkInternalFilters = wa_linkInternalFilters;
+
+s.channel = wa_channel;
+s.events = 'event1';
+var s_code = s.t(); if (s_code) document.write(s_code);
+
+var NCIAnalytics = {
+    SiteSearch: function(sender) {
+        var s = s_gi(s_account);
+        s.linkTrackVars = 'channel,prop14,eVar14,events';
+        s.linkTrackEvents = 'event2';
+        s.prop14 = sender.elements[0].value;
+        s.eVar14 = sender.elements[0].value;
+        s.channel = wa_channel;
+        s.events = 'event2';
+        s.tl(this, 'o', wa_search_function_name);
+    }
+};
