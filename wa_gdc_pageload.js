c89343c92b024cbff08a8fe6c44cc5f7717c3ba2
Dion D'aquino <daquinohd@users.noreply.github.com>
2017-01-23 14:56:57 +0000

    Merging 'gdc' branch back into Trunk

---
+++ wa_gdc_pageload.js
@@ -0,0 +1,7 @@
+// Version 2.1
+document.write('<script type="text/javascript" src="https://static.cancer.gov/webanalytics/wa_gdc_pre.js" ></script>');
+document.write('<script type="text/javascript" src="https://static.cancer.gov/webanalytics/s_code.js" ></script>');
+document.write('<script type="text/javascript" src="https://static.cancer.gov/webanalytics/wa_gdc_post.js" ></script>');
+
+// Add the line below just before the end body tag to execute Omniture page-load event:
+// <script language="JavaScript" type="text/javascript" src="https://static.cancer.gov/webanalytics/wa_gdc_pageload.js"></script>
