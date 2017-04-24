6201e0f7b385ed68a02a6a0d446226db8ee1dcf7
Dion D'aquino <daquinohd@users.noreply.github.com>
2017-04-24 13:43:11 +0000

    //tracker.nci.nih.gov/browse/OCEWAQ-971 - added CBIIT Javascripts

---
+++ wa_cbiit_pageload.js
@@ -0,0 +1,7 @@
+// Load site-specific and s_code analytics Javscript files for CBIIT sites
+document.write('<script type="text/javascript" src="https://static.cancer.gov/webanalytics/wa_cbiit_pre.js" ></script>');
+document.write('<script type="text/javascript" src="https://static.cancer.gov/webanalytics/s_code.js" ></script>');
+document.write('<script type="text/javascript" src="https://static.cancer.gov/webanalytics/wa_cbiit_post.js" ></script>');
+
+// Insert this on every page just above the closing body tag:
+// <script language="JavaScript" type="text/javascript" src="https://static.cancer.gov/webanalytics/wa_cbiit_pageload.js"></script>
---
+++ wa_cbiit_pre.js
@@ -0,0 +1,31 @@
+﻿/*
+* Set Web Analytics variables for for CSSI sites
+*/
+var wa_production_report_suite = 'nciglobal';
+var wa_dev_report_suite = 'ncidev';
+var wa_channel = '';
+var wa_search_function_name = '';
+var wa_production_url_match = '';
+var wa_linkInternalFilters = 'javascript:,';
+var currentHostname = location.hostname.toLowerCase();
+wa_linkInternalFilters += currentHostname;
+var page_URL = document.URL;
+
+// Set channel, search name, and prod URL 
+if (page_URL.indexOf('cbiitsolutions.cancer.gov') != -1) {	
+    wa_channel = 'CBIIT Solutions - Main';
+    wa_search_function_name = 'CBIIT Solutions - Search';
+    wa_production_url_match = 'cbiitsolutions.cancer.gov';
+}
+else {	
+    wa_channel = 'CBIIT - Dev';
+    wa_search_function_name = 'CBIIT - Search Dev';
+}
+
+// Set Prod or Dev suite depending on URL
+if (page_URL.indexOf(wa_production_url_match) != -1 && wa_production_url_match.length > 0)
+    var s_account = wa_production_report_suite;
+else
+    var s_account = wa_dev_report_suite;
+
+var pageNameOverride = currentHostname + location.pathname.toLowerCase();
