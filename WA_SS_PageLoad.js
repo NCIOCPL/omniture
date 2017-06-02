31537510757ab1f4de5e149e7f919b4364c90fd6
Ben Ferry <ferrybl-nih@users.noreply.github.com>
2017-06-02 15:30:54 +0000

    [CGOV-3648] - Cleanup old unused analytics files.

--- WA_SS_PageLoad.js
+++
@@ -1,9 +0,0 @@
-
-// Version 2.1
-document.write('<script type="text/javascript" src="//static.cancer.gov/webanalytics/WA_SS_pre.js" ></script>');
-document.write('<script type="text/javascript" src="//static.cancer.gov/webanalytics/s_code.js" ></script>');
-document.write('<script type="text/javascript" src="//static.cancer.gov/webanalytics/WA_SS_post.js" ></script>');
-document.write('<script language="javascript" id="_fed_an_ua_tag" src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=HHS&subagency=NCI"></script>');
-
-// Insert this on every page just above the closing body tag:
-// <script language="JavaScript" type="text/javascript" src="//static.cancer.gov/webanalytics/WA_SS_PageLoad.js"></script>
