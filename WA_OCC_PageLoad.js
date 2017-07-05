55cde6c576b3d9f8448736a769ef152c593f6972
Ben Ferry <ferrybl-nih@users.noreply.github.com>
2017-07-05 15:59:58 +0000

    [WCMSFEQ-581] Loading Universal Analytics asynchronously to avoid google chrome warning.

--- WA_OCC_PageLoad.js
+++ WA_OCC_PageLoad.js
@@ -2,7 +2,11 @@
 document.write('<script type="text/javascript" src="//static.cancer.gov/webanalytics/WA_OCC_pre.js" ></script>');
 document.write('<script type="text/javascript" src="//static.cancer.gov/webanalytics/s_code.js" ></script>');
 document.write('<script type="text/javascript" src="//static.cancer.gov/webanalytics/WA_OCC_post.js" ></script>');
-document.write('<script language="javascript" id="_fed_an_ua_tag" src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=HHS&subagency=NCI"></script>');
+var s = document.createElement('script');
+s.type='text/javascript';
+s.id="_fed_an_ua_tag";
+s.src='https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=HHS&subagency=NCI';
+document.head.appendChild(s);
 
 // Insert this on every HTML page just above the closing body tag:
 // <!-- **** NCI Web Analytics - DO NOT ALTER **** -->
