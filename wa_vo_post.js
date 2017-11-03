25a20224b5ed1658d4f6eb3d5d24cda6ba9856b2
Dion <daquinohd@users.noreply.github.com>
2017-11-03 10:13:20 -0400

    Adding utility functions to wa_vo_post

--- wa_vo_post.js
+++ wa_vo_post.js
@@ -5,6 +5,8 @@ s.events = 'event1';
 var s_code = s.t(); if (s_code) document.write(s_code);
 
 var NCIAnalytics = {
+    
+    // Legacy site search function
     SiteSearch: function(sender) {
         var s = s_gi(s_account);
         s.linkTrackVars = 'channel,prop14,eVar14,events';
@@ -14,5 +16,44 @@ var NCIAnalytics = {
         s.channel = wa_channel;
         s.events = 'event2';
         s.tl(this, 'o', wa_search_function_name);
+    },
+    
+    // Function to track search dropdown, more search options
+    SearchOptions: function(sender, value) {
+        // Placeholder 
+    },
+
+    // Function to track call to action links on details pages
+    DetailsActionClick: function(sender, value) {
+        var s = s_gi(s_account);
+        s.linkTrackVars = 'channel,prop5,events';
+        s.linkTrackEvents = 'event59';
+        s.prop5 = value;
+        s.channel = wa_channel;        
+        s.events = 'event59';
+        s.tl(this, 'o', 'DetailsActionClick');    
+    },
+
+    // Function to track call to action links on "My Photos" pages
+    FavoritesActionClick: function(sender, value) {
+        var s = s_gi(s_account);
+        s.linkTrackVars = 'channel,prop5,events';
+        s.linkTrackEvents = 'event61';
+        s.prop5 = value;
+        s.channel = wa_channel;        
+        s.events = 'event61';
+        s.tl(this, 'o', 'DetailsActionClick');    
+    },
+      
+    // Function to track button clicks on details pages
+    UtilityLinkClick: function(sender, value) {
+        var s = s_gi(s_account);
+        s.linkTrackVars = 'channel,prop5,events';
+        s.linkTrackEvents = 'event60';
+        s.prop5 = value;
+        s.channel = wa_channel;        
+        s.events = 'event60';
+        s.tl(this, 'o', 'UtilityLinkClick');    
     }
+    
 };
